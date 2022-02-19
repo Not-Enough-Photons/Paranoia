326eef99a2422b8a050b875b0c0cd6f20cce799d
adamdev <hmullinsk@hotmail.com>
2022-02-18 22:37:21 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -22,7 +22,7 @@ namespace NEP.Paranoia
 		public const string Description = "Stay away from there. Please."; // Description for the Mod.  (Set as null if none)
 		public const string Author = "Not Enough Photons"; // Author of the Mod.  (MUST BE SET)
 		public const string Company = "Not Enough Photons"; // Company that made the Mod.  (Set as null if none)
-		public const string Version = "3.0.0"; // Version of the Mod.  (MUST BE SET)
+		public const string Version = "3.0.2"; // Version of the Mod.  (MUST BE SET)
 		public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
 	}
 
